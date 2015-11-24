be7332f4d8fd04c1c3640da52c83671756301584
Emil Mork <emil.a.mork@gmail.com>
2015-11-24 22:34:36 +0100

    Remove es6 class syntax. Several new features
    - Automatic key detection. Removes the need for keys in data.
    - Make it possible to add initial data

--- src/index.js
+++ src/index.js
@@ -1,80 +1,140 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import c3 from 'c3';
 import merge from 'deepmerge';
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {chart: null};
+const isDate = (key) => key === "date";
+const isList = (data) => data && data.length;
+const emptyList = (list) => isList(data) && list.length > 1;
+const hasDataProperty = (data) => data.hasOwnProperty('date');
 
+var RTChart = React.createClass({
+
+  componentDidMount: function() {
     this.limit = (this.props.maxValues || 10);
-    this.count = 0;
-  }
+    this.count = this.props.initialData || 0;
+    this.ids = this.props.fields;
 
-  componentDidMount() {
-    this.initChart(); 
-  }
+    this.initChart();
+  },
 
-  componentWillReceiveProps(nextProps) {
-    if(!this.state.chart) return;
-    if(!nextProps.data || !nextProps.data.values) return;
-    if(nextProps.data.values.length < this.props.fields.length) {
-      throw new Error(`Values has a length of ${nextProps.values.length} but must be the same as fields: ${this.props.fields.length}`);
+  getInitialState: function() {
+    return {
+      chart: null
+    };
+  },
+
+  loadHistoryData: function(historicalData) {
+    if (historicalData.length > this.limit) {
+        historicalData = historicalData.slice(historicalData.length - this.limit);
+        this.count = this.limit;
     }
 
-    var columns = [['x', nextProps.data.date]];
-    var vals = nextProps.data.values.forEach((val) => {
-      columns.push([val.id, val.value]);
+    var columnData = { 'date': []};
+    this.props.fields.forEach(f => columnData[f] = []);
+
+    historicalData.forEach((value) => {
+      let i = 0;
+      for (let key of Object.keys(value)) {
+        columnData[key].push(isDate(key) ? new Date(value[key]) : value[key]);
+        i++;
+      }
     });
+
+    var columns = [];
+    for (let key of Object.keys(columnData)) {
+      if (key == "date") columns.push(['x'].concat(columnData[key]));
+      else columns.push([key].concat(columnData[key]));
+    }
+
+    return columns;
+  },
+
+  unload() {
+    this.state.chart.unload({
+      ids: this.props.fields
+    });
+  },
+
+  resetChart: function() {
+    this.unload();
+    this.initChart();
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    if (!this.state.chart) return;
+    if (!nextProps.data) return;
+    if (Object.keys(nextProps.data).length < this.props.fields.length) {
+      throw new Error(
+        `Values has a length of ${nextProps.values.length} but must be the same as fields: ${this.props.fields.length}`
+      );
+    }
+
+    if (nextProps.reset) {
+      this.resetChart();
+    }
+
+    var columns = this.loadHistoryData([nextProps.data]);
+
     var args = merge({
-      columns: columns,
-      duration: 350
-      }, (this.props.flow || {}));
+                columns: columns,
+                duration: 400
+              }, (this.props.flow || {}));
 
-    if(this.count <= this.limit) this.count++;
+    if (this.count <= this.limit) this.count++;
 
-    if(this.count < this.limit) args['length'] = 0;
+    if (this.count < this.limit) args['length'] = 0;
+
     this.state.chart.flow(args);
-  }
+  },
 
-  render() {
-    return <div style={this.props.style} ref='chart'/>
-  }
+  render: function() {
+    return <div style = { this.props.style } ref='chart'/>
+  },
 
-  initChart() {
-    if(!this.props.fields) {
-      throw new Error("prop type fields are missing. fields={['field',..]}");
+  initChart: function() {
+    if (!this.props.fields) {
+      throw new Error(
+        "prop type fields are missing. fields={['field',..]}");
       return;
     }
-    var columns = [['x']];
-    this.props.fields.forEach((f) => columns.push([f]));
+    var defaultColumns = [
+      ['x']
+    ];
+    this.props.fields.forEach((f) => defaultColumns.push([f]));
 
     var chart_temp = merge({
       axis: {
-            x: {
-                type: 'timeseries',
-                tick: {
-                    format: '%H:%M:%S',
-                }
-            }
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%H:%M:%S',
+          }
         }
+      }
     }, (this.props.chart || {}));
 
-    // Override potential errors. Need bindto and axis.x.type to be timeseries
-    chart_temp.bindto = React.findDOMNode(this.refs.chart);
-    chart_temp.data = {x: 'x', columns: columns };
+    chart_temp.bindto = ReactDOM.findDOMNode(this);
+    chart_temp.data = {
+      x: 'x',
+      columns: this.loadHistoryData(this.props.historicalData) || defaultColumns
+    };
     chart_temp.axis.x.type = 'timeseries';
 
     var chart = c3.generate(chart_temp);
 
-    this.setState({chart: chart});
-  }
+    this.setState({
+      chart: chart,
+      historicalData: this.props.historicalData
+    });
+  },
 
   propTypes: {
     dateFormat: React.PropTypes.string,
     chart: React.PropTypes.object,
-    fields: React.PropTypes.object.isRequired,
+    fields: React.PropTypes.array.isRequired,
     maxValues: React.PropTypes.number
   }
-}
+});
 
+module.exports = RTChart;
